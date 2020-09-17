51977b32a7b6dfcdf88be4281104ed13479a16a2
Dylan DE SOUSA <dds1991@hotmail.fr>
2020-09-17 08:18:50 +0200

    ex02 & ex03

---
+++ ex02.js
@@ -0,0 +1,18 @@
+function upperCase(str = '') {
+  let sentence = ''
+
+  for(let i = 0; i < str.length; i++) {
+    const code = str.charCodeAt(i)
+
+    if (code >= 97 && code <= 122) {
+      sentence += String.fromCharCode(code - 32)
+    } else {
+      sentence += String.fromCharCode(code)
+    }
+  }
+
+  return sentence
+}
+
+console.log(upperCase('Chopper'))
+console.log(upperCase('Tony Tony'))
---
+++ ex03.js
@@ -0,0 +1,11 @@
+function length(str) {
+    let i = 0
+
+    while(str[i] != undefined) {
+        i++
+    }
+
+    return i
+}
+
+console.log(length('Bon'))
